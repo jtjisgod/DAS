f89a36701b03591da432f76d493c9b0243845b54
yoonjungjun0202 <love667@naver.com>
2017-10-18 15:36:49 -0400

    Add SSLStript.py Function, ip_forward

--- SSLStrip.py
+++ SSLStrip.py
@@ -1,10 +1,36 @@
 from CommandModule import *
+from scapy.all import *
+import subprocess
 
 class SSLStrip(CommandModule) :
     command = "ssl"
-    outline = "-"
-    manual = ""
+    outline = "This is a command which is stripping 'https://' and turning them into 'http://'"
+    manual = "This command can sniff the data which communicate on ssl"
 
     def run(self) :
         print("THIS IS SSL-STRIP")
+        res = self.ipForward()
+        print(res)
         pass
+
+    def sslStrip(self) :
+        pass
+
+    # IP Forwarding : be router.
+    def ipForward(self) :
+        ipForwardPath = "/proc/sys/net/ipv4/ip_forward"
+        ipForwardCommand = "echo 1 > " + ipForwardPath
+        checkIpForwardCommand = "cat " + ipForwardPath
+
+        subprocess.run(ipForwardCommand, shell=True)
+        res = int(subprocess.check_output(checkIpForwardCommand, shell=True))
+        return (0 == res)
+
+    def ipTable(self) :
+        pass
+
+    def arpSpoofing(self) :
+        pass
+
+sslStrip = SSLStrip()
+sslStrip.run()
