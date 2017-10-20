bc1b8c32ca4c9f92dfb7ad25c040ebc2e46f750e
yoonjungjun0202 <love667@naver.com>
2017-10-20 02:44:53 -0400

    Split class

--- SSLStrip.py
+++ SSLStrip.py
@@ -1,23 +1,19 @@
 from CommandModule import *
-# from scapy.all import *
+from scapy.all import *
+from time import sleep
 import subprocess
 
 class SSLStrip(CommandModule) :
     command = "ssl"
     outline = "This is a command which is stripping 'https://' and turning them into 'http://'"
     manual = "This command can sniff the data which someone communicates on ssl"
-    kIpForwardPathString = "/proc/sys/net/ipv4/ip_forward"
-
 
     def run(self) :
-        print("## SSL-STRIP Start...")
-        self.ipForward()
-        if False == self.checkIpForward() :
-            pass
+        if False == NetSetup.checkIpForward() :
+            NetSetup.ipForward()
 
-        self.ipTables()
-        if False == self.checkIpTables() :
-            pass
+        if False == NetSetup.checkIpTables() :
+            NetSetup.ipTables()
 
         # ssl-strip & arp
         pass
@@ -27,11 +23,16 @@ class SSLStrip(CommandModule) :
         pass
 
 
+
+
+class NetSetup() :
+
     # IP Forwarding : be router.
     # If IP Forward is set, all packet allowed.
-    def ipForward(self) :
-        print("# IP Forawrding...")
-        ipForwardCommand = "echo 1 > " + self.kIpForwardPathString
+    @staticmethod
+    def ipForward() :
+        print("# Set IP Forwarding...")
+        ipForwardCommand = "echo 1 > /proc/sys/net/ipv4/ip_forward"
         try :
             subprocess.run(ipForwardCommand, shell=True)
             sleep(1)
@@ -40,9 +41,10 @@ class SSLStrip(CommandModule) :
 
 
     # Check if IP Forward is set.
-    def checkIpForward(self) :
+    @staticmethod
+    def checkIpForward() :
         print("# Check IP Forawrding...")
-        checkIpForwardCommand = "cat " + self.kIpForwardPathString
+        checkIpForwardCommand = "cat /proc/sys/net/ipv4/ip_forward"
         try :
             res = (1 == int(subprocess.check_output(checkIpForwardCommand, shell=True)))
         except Exception as e:
@@ -52,19 +54,22 @@ class SSLStrip(CommandModule) :
 
     # IP Table : packet filtering tool(= firewall).
     # It doesn't filter the packet, it is just the rule of filtering packet.
-    def ipTables(self) :
+    @staticmethod
+    def ipTables(listenPort=10000) :
         print("# Add a rule to IP Table...")
-        ipTableCommand  = "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 10000"
+        ipTableCommand  = "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port " + str(listenPort)
         try :
             subprocess.run(ipTableCommand, shell=True)
             sleep(1)
         except Exception as e:
             pass
 
+
     # Check if IP Table is added our redirection(port 80 to 10000) rules.
-    def checkIpTables(self) :
+    @staticmethod
+    def checkIpTables(listenPort=10000) :
         print("# Check IP Tables...")
-        checkIpTableCommand = "iptables -t nat -L -v -n | grep \"dpt:80 redir ports 10000\""
+        checkIpTableCommand = "iptables -t nat -L -v -n | grep \"dpt:80 redir ports " + str(listenPort)+"\""
         try :
             res = (None != subprocess.check_output(checkIpTableCommand, shell=True))
         except Exception as e:
@@ -72,8 +77,6 @@ class SSLStrip(CommandModule) :
         return res
 
 
-    def arpSpoofing(self) :
-        pass
 
 sslStrip = SSLStrip()
 sslStrip.run()
