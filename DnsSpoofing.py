64b6ac41057764dbda9863dfd7dd13bef7c5ff4f
yoonjungjun0202 <love667@naver.com>
2017-10-24 12:06:41 -0400

    iptables...modify

--- DnsSpoofing.py
+++ DnsSpoofing.py
@@ -2,6 +2,7 @@ from CommandModule import *
 from scapy.all import *
 from SSLStrip import NetSetup
 from netfilterqueue import NetfilterQueue
+import _thread
 
 class DnsSpoofing(CommandModule) :
     command = "dns"
@@ -33,11 +34,14 @@ class DnsSpoofing(CommandModule) :
         iptablesOpt = "-t nat -A PREROUTING -p udp --sport 53 -j NFQUEUE --queue-num " + str(responseID)
         if False == NetSetup.getInstance().checkIpTables(checkOpt) :
             NetSetup.getInstance().ipTables(iptablesOpt)
-        # iptablesOpt = "-I INPUT -d 192.168.0.8/24 -j NFQUEUE --queue-num " + str(responseID)
-        # NetSetup.getInstance().ipTables(iptablesOpt)
 
 
         # DNS-Spoofing
+        # _thread.start_new_thread(self.dnsCachePoisoning, (queryID, responseID))
+        self.dnsCachePoisoning(queryID, responseID)
+
+
+    def dnsCachePoisoning(self, queryID, responseID) :
         print("# DNS cache poisoning...")
         # sniff(lfilter=lambda x: x.haslayer(DNS), prn=processDns)
         try :
@@ -53,6 +57,7 @@ class DnsSpoofing(CommandModule) :
     def dnsQueryCallback(self, packet) :
         # pkt = IP(packet.get_payload)
         # packet.set_payload(str(pkt))
+        print("# DNS Query")
         print(packet)
         packet.accept()
         pass
@@ -61,8 +66,9 @@ class DnsSpoofing(CommandModule) :
     def dnsResponseCallback(self, packet) :
         # pkt = IP(packet.get_payload)
         # packet.set_payload(str(pkt))
-        packet.accept()
+        print("# DNS Response")
         print(packet)
+        packet.accept()
         pass
 
 
@@ -76,20 +82,20 @@ def processDns(packet) :
     # DNSRR : DNS Resource Record
     elif DNSRR in packet and packet.sport == 53 :
         print("# response...")
-        payload = IP(packet.get_payload())
+        # payload = IP(packet.get_payload())
     else :
         print("It is not a DNS packet")
         pass
 
-    if IP in pkt :
-        ip_src = pkt[IP].src
-        ip_dst = pkt[IP].dst
-        if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0 :
-            print(str(ip_src) + " -> " + str(ip_dst) + " : " + "(" + str(pkt.getlayer(DNS).qd.qname) + ")")
-        elif pkt.haslayer(DNS) and pkt.getlayer(DNS).rr == 0 :
-            print(str(ip_src) + " -> " + str(ip_dst) + " : " + "(" + str(pkt.getlayer(DNS).qd.qname) + ")")
-
+    if IP in packet :
+        ip_src = packet[IP].src
+        ip_dst = packet[IP].dst
+        if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0 :
+            print(str(ip_src) + " -> " + str(ip_dst) + " : " + "(" + str(packet.getlayer(DNS).qd.qname) + ")")
+        elif packet.haslayer(DNS) and packet.getlayer(DNS).rr == 0 :
+            print(str(ip_src) + " -> " + str(ip_dst) + " : " + "(" + str(packet.getlayer(DNS).qd.qname) + ")")
 """
+
 
 
 if __name__ == '__main__' :
